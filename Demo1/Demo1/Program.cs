ab9a7f2f0330b73b0de138fb68a17eca419b3717
Areart <15778250484@163.com>
2018-11-04 08:49:20 +0800

    DB first

---
+++ Demo1/Demo1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var dep=new CuresContext())
+            {
+                foreach (var d in dep.Departments.OrderBy(x=>x.SortCode).ToList())
+                {
+                    Console.WriteLine("编号：{0},名称：{1},说明{2}",d.SortCode,d.Name,d.Dscn);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
